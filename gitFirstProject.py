4731db5264275f153ca25df2f9d3132e19f9e5fb
DanialZash <danialzash@gmail.com>
2022-01-07 03:49:43 +0330

    class method vs static method polymorphism static variable
    are added.

--- gitFirstProject.py
+++ gitFirstProject.py
@@ -6,3 +6,4 @@ how to use with pycharm and github
 
 print('hello world')
 
+#comment
--- pythonOOP/polymorphismEx.py
+++ pythonOOP/polymorphismEx.py
@@ -13,47 +13,56 @@ def add(x, y, z=0):
 print(add(2, 3))
 print(add(2, 3, 4))
 
-class India():
-	def capital(self):
-		print("New Delhi is the capital of India.")
 
-	def language(self):
-		print("Hindi is the most widely spoken language of India.")
+class India:
+    def capital(self):
+        print("New Delhi is the capital of India.")
 
-	def type(self):
-		print("India is a developing country.")
+    def language(self):
+        print("Hindi is the most widely spoken language of India.")
+
+    def type(self):
+        print("India is a developing country.")
+
 
 class USA():
-	def capital(self):
-		print("Washington, D.C. is the capital of USA.")
+    def capital(self):
+        print("Washington, D.C. is the capital of USA.")
+
 
 	def language(self):
 		print("English is the primary language of USA.")
 
-	def type(self):
-		print("USA is a developed country.")
+
+    def type(self):
+        print("USA is a developed country.")
+
 
 obj_ind = India()
 obj_usa = USA()
 for country in (obj_ind, obj_usa):
-	country.capital()
-	country.language()
-	country.type()
+    country.capital()
+    country.language()
+    country.type()
 
 
 class Bird():
     def intro(self):
         print("birds")
+
     def flight(self):
         print("birds can fly")
+
 
 class Sparrow(Bird):
     def flight(self):
         print("Sparrow can fly")
 
+
 class Ostrich(Bird):
     def flight(self):
         print("ostrich can't fly")
+
 
 obj_bird = Bird()
 obj_sparrow = Sparrow()
