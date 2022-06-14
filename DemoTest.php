bf8f1c5fd23c6069f944fcfb0db63538630b21ce
DanialZash <danialzash@gmail.com>
2022-06-15 00:10:03 +0430

    another userName

--- DemoTest.php
+++ DemoTest.php
@@ -2,10 +2,13 @@
 
 class DemoTest
 {
+    private static $simpleValue = 10;
+
     public function __construct()
     {
         echo "it's a constructor :D";
         echo 'test';
         echo 'git name has changed';
     }
+
 }
