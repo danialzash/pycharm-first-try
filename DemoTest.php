9cefda72672eaba46c1e1e45926686d33b45b46d
ZASH <danialzash@gmail.com>
2022-06-17 19:51:14 +0430

    add new form

--- DemoTest.php
+++ DemoTest.php
@@ -1,22 +1,23 @@
 <?php
 
-class DemoTest
+$user = 'root';
+$password = 'root';
+$db = 'DBTest';
+$host = 'localhost';
+$port = 8889;
+
+$link = mysqli_init();
+$success = mysqli_real_connect($link, $host, $user, $password, $db, $port);
+
+$ans = $link->query("select * from customer");
+
+$strAns = $ans->fetch_all();
+
+foreach ($strAns as $item)
 {
-    private static $simpleValue = 10;
-
-    public function __construct()
-    {
-        echo "it's a constructor :D";
-        echo 'test';
-        echo 'git name has changed';
-    }
-
-    /**
-     * @return int
-     */
-    public static function getSimpleValue()
-    {
-        return self::$simpleValue;
-    }
-
+    echo "$item[1] $item[2] </br>";
 }
+$link->close();
+
+echo "<h1>" . $_POST['name']."</h1>";
+echo $_POST['lname'];
---
+++ simpleForm.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form action="DemoTest.php" method="POST">
+            <p>Name: <input type="text" name="name"></p>
+            <p>LastName: <input type="password" name="lname"></p>
+            <p><input type="submit" value="send"></p>
+        </form>
+    </body>
+</html>
